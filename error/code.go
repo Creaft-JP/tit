db46bb847e5c81798feb6d4a063543879d8d9b97
たけちゃん <79554611+takechan-NITNC@users.noreply.github.com>
2023-08-27 16:36:41 +0900

    define error code

---
+++ error/code.go
@@ -0,0 +1,9 @@
+package error
+
+import "github.com/morikuni/failure"
+
+const File failure.StringCode = "File"
+const Network failure.StringCode = "Network"
+const Operation failure.StringCode = "Operation"
+const Database failure.StringCode = "Database"
+const Authentication failure.StringCode = "Authentication"
