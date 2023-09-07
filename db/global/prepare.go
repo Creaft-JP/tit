154611a819755880560b67e64e4e27ad9cf52b5b
たけちゃん <79554611+takechan-NITNC@users.noreply.github.com>
2023-09-07 21:29:34 +0900

    create functions which prepare a global database

---
+++ db/global/prepare.go
@@ -0,0 +1,31 @@
+package global
+
+import (
+	"context"
+	"entgo.io/ent/dialect"
+	"fmt"
+	"github.com/Creaft-JP/tit/db/local/ent"
+	e "github.com/Creaft-JP/tit/error"
+	"github.com/Creaft-JP/tit/global"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/morikuni/failure"
+	"path/filepath"
+)
+
+var FilePath = filepath.Join(global.Path, "database")
+
+// MakeClient make SQLite3 ent Client from path
+//
+// if there isn't file on the filePath, automatically make new file
+func MakeClient(filePath string) (*ent.Client, error) {
+	name := fmt.Sprintf("%s?_fk=1", filePath)
+	client, err := ent.Open(dialect.SQLite, name)
+	return client, failure.Translate(err, e.Database)
+}
+
+func Migrate(client *ent.Client, ctx context.Context) error {
+	if err := client.Schema.Create(ctx); err != nil {
+		return failure.Translate(err, e.Database)
+	}
+	return nil
+}
